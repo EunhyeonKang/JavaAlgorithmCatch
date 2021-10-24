aedac32b9669904b386816a388b94f7196d15adf
EunhyeonKang <rkddmsgus1@daum.net>
2021-10-24 23:51:10 +0900

    setting

--- src/week1/arr_1.java
+++ src/week1/arr_1.java
@@ -1,6 +1,6 @@
 package week1;
 
-public class question1 {
+public class arr_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/week1/arr_2.java
+++ src/week1/arr_2.java
@@ -1,6 +1,6 @@
 package week1;
 
-public class question2 {
+public class arr_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

14405f41e3b3bc77f465aae5a21f21878c2b4c3c
jy-cheon <emily.blunt983@gmail.com>
2021-10-25 18:51:22 +0900

    1431.

--- src/week1/arr_1.java
+++ src/week1/arr_1.java
@@ -1,10 +1,31 @@
 package week1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// 1431. Kids With the Greatest Number of Candies
 public class arr_1 {
+	public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+		List<Boolean> res = new ArrayList<>();
+		int max = 0;
 
+		for (int n : candies) {
+			if (n > max) {
+				max = n;
+			}
+		}
+
+		for (int n : candies) {
+			if (n + extraCandies >= max) {
+				res.add(true);
+			} else {
+				res.add(false);
+			}
+		}
+		return res;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(kidsWithCandies(new int[]{2,3,5,1,3}, 3));
 	}
 
 }

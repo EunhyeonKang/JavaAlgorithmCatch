22617fef3b5b1be4540e105ef753ae6158931499
EunhyeonKang <rkddmsgus1@daum.net>
2021-10-26 22:00:35 +0900

    1431 solved

--- src/week1/arr_1.java
+++ src/week1/arr_1.java
@@ -1,10 +1,27 @@
 package week1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class arr_1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = { 2, 3, 5, 1, 3 };
+		int extraCandies = 3;
+		kidsWithCandies(arr, extraCandies);
+	}
 
+	public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+		List<Boolean> result = new ArrayList<>();
+		int maxChild = 0;
+		for (int x : candies)
+			maxChild = Math.max(maxChild, x);
+		for (int x : candies) {
+			if (x + extraCandies >= maxChild)
+				result.add(true);
+			else
+				result.add(false);
+		}
+		return result;
 	}
 
 }

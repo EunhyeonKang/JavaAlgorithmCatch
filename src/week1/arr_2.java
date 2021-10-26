260fd7e1799403d29b34e116aa1ecd9f68fc6e00
jy-cheon <emily.blunt983@gmail.com>
2021-10-26 18:44:04 +0900

    1184.

--- src/week1/arr_2.java
+++ src/week1/arr_2.java
@@ -1,9 +1,27 @@
 package week1;
 
+// 1184. Distance Between Bus Stops
 public class arr_2 {
+	public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+		int front = 0;
+		int back = 0;
+
+		if(start > destination) {
+			int tmp = start;
+			start = destination;
+			destination = tmp;
+		}
+		for (int i=0; i<distance.length; i++) {
+			if (start <= i && i < destination) {
+				front += distance[i];
+			} else {
+				back += distance[i];
+			}
+		}
+		return Math.min(front, back);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 

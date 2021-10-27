668847f701889d74ade69bf58dfc2b7fc5ea3b71
EunhyeonKang <rkddmsgus1@daum.net>
2021-10-27 17:46:07 +0900

    1184 solved

--- src/week1/arr_2.java
+++ src/week1/arr_2.java
@@ -3,8 +3,26 @@ package week1;
 public class arr_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] distance = { 7,10,1,12,11,14,5,0};
+		int start = 7, destination = 2;
+		System.out.println(distanceBetweenBusStops(distance, start, destination));
 	}
 
+	public static int distanceBetweenBusStops(int[] distance, int start, int destination) {
+		int n = distance.length;
+		int i = (start+1)%n;
+		int sum=distance[start];
+		int min = Integer.MAX_VALUE;
+		while(i!=start) {
+			if(i==destination) {
+				min = Math.min(min, sum);
+				sum=0;
+			}
+			sum+=distance[i];
+			i = (i+1)%n;
+		}
+		min = Math.min(min, sum);
+		
+		return min;
+	}
 }
